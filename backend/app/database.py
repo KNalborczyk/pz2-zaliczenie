b118d666cc6365a20b29a818d24564dc353be4c1
Krzysztof Nalborczyk <knalborczyk2003@gmail.com>
2026-05-25 22:24:07 +0200

    fix: correct PostgreSQL database name

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/currencies"
+DATABASE_URL = "postgresql://postgres:postgres@db:5432/currency_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
